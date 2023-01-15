7ed9d773cfd06d60eb203e550a7aa63c459eef79
batihanozio <vexedeity@DESKTOP-6VJ2J89>
2023-01-15 20:00:28 +0300

    first day

---
+++ components/Row.tsx
@@ -0,0 +1,54 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
+import { useRef, useState } from "react";
+import { Movie } from "../typings";
+import Thumbnail from "./Thumbnail";
+
+interface Props {
+  title: string;
+  movies: Movie[];
+  //   when using firebaseV
+  //   movie: Movie  | DocumentData
+}
+function Row({ title, movies }: Props) {
+  const rowRef = useRef<HTMLDivElement>(null);
+  const [isMoved, setIsMoved] = useState(false);
+
+  const handleClick = (direction: string) => {
+    setIsMoved(true);
+    if (rowRef.current) {
+      const { scrollLeft, clientWidth } = rowRef.current;
+      const scrollTo =
+        direction === "left"
+          ? scrollLeft - clientWidth
+          : scrollLeft + clientWidth;
+      rowRef.current.scrollTo({ left: scrollTo, behavior: "smooth" });
+    }
+  };
+  return (
+    <div className="h-40 space-y-0.5 md:space-y-2">
+      <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
+        {title}
+      </h2>
+      <div className="group relative md:-ml-2">
+        <ChevronLeftIcon
+          className={`arrowIcons ${!isMoved && "hidden"} left-2`}
+          onClick={() => handleClick("left")}
+        />
+        <div
+          ref={rowRef}
+          className="flex scrollbar-hide items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2 scroll-auto"
+        >
+          {movies.map((movie) => (
+            <Thumbnail key={movie.id} movie={movie} />
+          ))}
+        </div>
+        <ChevronRightIcon
+          className={`arrowIcons right-2`}
+          onClick={() => handleClick("right")}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Row;
