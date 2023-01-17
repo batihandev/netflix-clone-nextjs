c3fc3e2f9546fa25fc757a921431fafc39236900
batihanozio <vexedeity@DESKTOP-6VJ2J89>
2023-01-17 20:21:00 +0300

    fixed scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,11 @@ const Home = ({
   const showModal = useRecoilValue(modalState);
   if (loading) return null;
   return (
-    <div className="relative h-screen bg-gradient-to-b lg:h-[140v]">
+    <div
+      className={`relative h-screen bg-gradient-to-b lg:h-[140v]${
+        showModal && "!h-screen overflow-hidden"
+      } `}
+    >
       <Head>
         <title>Home -Netflix</title>
         <link rel="icon" href="/favicon.ico" />
